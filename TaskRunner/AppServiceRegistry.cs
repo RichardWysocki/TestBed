f541862b1266f9553f25d5da9d07da8a6ab9d51f
Richad Wysocki <RichardWysocki@gmail.com>
2020-05-07 23:12:27 -0400

    Update Files for DI with Lamar

---
+++ TaskRunner/AppServiceRegistry.cs
@@ -0,0 +1,20 @@
+﻿using Lamar;
+using TaskRunner.ValidationRules;
+
+namespace TaskRunner
+{
+    public class AppServiceRegistry : ServiceRegistry
+    {
+        public AppServiceRegistry()
+        {
+           Scan(x =>
+                {
+                    x.Assembly(typeof(Program).Assembly);
+                    x.AddAllTypesOf<ITaskValidation>().NameBy(type => type.Name.ToLower());
+                    x.AddAllTypesOf<IFantasySeries>().NameBy(type => type.Name.ToLower());
+                    x.WithDefaultConventions();
+
+                });
+        }
+    }
+}
--- TaskRunner/Program.cs
+++ TaskRunner/Program.cs
@@ -10,46 +10,45 @@ namespace TaskRunner
     {
         static void Main(string[] args)
         {
+
+
             var registry = new ServiceRegistry();
+            registry.IncludeRegistry<AppServiceRegistry>();
 
             //setup our DI
-            var container = new Container(x =>
-            {
-                // Using StructureMap style registrations
-                //x.Scan(x =>
-                //{
-                //    x.Assembly(typeof(Program).Assembly);
-                //    x.WithDefaultConventions();
+            var container = new Container(registry);
 
-                //});
-                x.Scan(x =>
-                {
-                    x.Assembly(typeof(Program).Assembly);
-                    x.AddAllTypesOf<ITaskValidation>().NameBy(type => type.Name.ToLower());
-                    x.AddAllTypesOf<IFantasySeries>().NameBy(type => type.Name.ToLower());
-                    x.WithDefaultConventions();
-                   
-                });
-                //x.For<IEnumerable<ITaskRun>>().Add(x => x.GetServices<ITaskRun>());
-                //x.For<ITaskRun>().Add<NameValidation>();
-                //x.For<ITaskRun>().Add<AddressValidation>();
+            //var container = new Container(x =>
+            //{
+
+            //    x.IncludeRegistry<AppServiceRegistry>();
+            //    x.Scan(x =>
+            //    {
+            //        x.Assembly(typeof(Program).Assembly);
+            //        x.AddAllTypesOf<ITaskValidation>().NameBy(type => type.Name.ToLower());
+            //        x.AddAllTypesOf<IFantasySeries>().NameBy(type => type.Name.ToLower());
+            //        x.WithDefaultConventions();
+
+            //    });
+            //    ////x.For<IEnumerable<ITaskRun>>().Add(x => x.GetServices<ITaskRun>());
+            //    ////x.For<ITaskRun>().Add<NameValidation>();
+            //    ////x.For<ITaskRun>().Add<AddressValidation>();
 
 
-                //x.For<ITaskRun>().Add(y => y.)
+            //    ////x.For<ITaskRun>().Add(y => y.)
 
 
-            });
-            
+            //});
 
+
+            Console.WriteLine(container.WhatDidIScan());
 
             var response = container.GetAllInstances<ITaskValidation>();
             Console.WriteLine("Hello World!");
 
-            var bar = container.GetService<IRunManager>();
-            bar.Run();
+            var runManager = container.GetService<IRunManager>();
+            runManager.Run();
 
-            //var x = new RunManager();
-            //x.Run();
             Console.WriteLine("Hello World!");
          }
     }
